388dfdd0ee7087db814225ff2cc4e46cebd00ef5
salman <salman@zennode.com>
2021-12-29 12:50:41 +0530

    third commit

--- app/__init__.py
+++ app/__init__.py
@@ -16,21 +16,6 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 migrate = Migrate()
-# opentok = OpenTok('47344281', 'a31d1885cb3e9f04869ab66ab128dba12eaa7e29')
-# opentok_api = '47344281'
-
-# #-------------- stripe ---------------------------------------#
-# stripe_keys = {
-#     "secret_key": os.getenv('STRIPE_SECRET_KEY'),
-#     "publishable_key": os.getenv('STRIPE_PUBLISHABLE_KEY'),
-# }
-# stripe.api_key = stripe_keys["secret_key"]
-
-# #-------------- stripe ---------------------------------------#
-
-
-# mail = Mail()
-# admins = Admin()
 
 def create_app(config_class=Config):
 	app = Flask(__name__)
@@ -40,18 +25,9 @@ def create_app(config_class=Config):
 	bcrypt.init_app(app)
 	login_manager.init_app(app)
 	migrate.init_app(app, db)
-	# mail.init_app(app)
-	# admins.init_app(app)
 
 	from app.users.routes import users
-	# from app.calling.routes import calling
-	# from app.products.routes import products
-	# from app.subscriptions.routes import subscriptions
+
 	app.register_blueprint(users)
-	# app.register_blueprint(calling)
-	# app.register_blueprint(products)
-	# app.register_blueprint(subscriptions)
-
-
 
 	return app
